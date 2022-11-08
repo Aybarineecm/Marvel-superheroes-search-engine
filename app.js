5389e3139824962021e6e2bcef0d70bd07dd81f0
Aybarineecm <aybarineecampbellmendez@gmail.com>
2022-11-07 20:43:54 -0500

    added ex 2

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-window.onload = function()
-{
+    const httpRequest = new XMLHttpRequest(); 
+    let url = "http://localhost/info2180-lab4/superheroes.php";
     let searchbutton = document.getElementById("search");
-    searchbutton.addEventListener("click",function() 
+    searchbutton.addEventListener("click",function(e)
     {
-        this.preventDefault();
-        get('http://localhost/info2180-lab4/superheroes.php').then(response => {
-            return response.text();
-        })
-        .then(response => {
-            alert(response);
-        })
+        e.preventDefault();
+        function doSomething() 
+      { 
+        
+        if (httpRequest.readyState === XMLHttpRequest.DONE)
+         {
+             if (httpRequest.status === 200)
+              { 
+                let response = httpRequest.responseText; alert(response); 
+              } 
+                else
+                 { 
+                    alert('There was a problem with the request.');  
+                } 
+            } 
+        }
+        httpRequest.onreadystatechange = doSomething; 
+        httpRequest.open('GET', url); 
+        httpRequest.send();
     })
-};
 
